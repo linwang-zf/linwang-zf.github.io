ac8d0665bb553695ae7b6a22264990de5c46966a
linwang-zf <903862530@qq.com>
2022-01-13 23:44:32 +0800

    Site updated: 2022-01-13 23:44:30

--- js/utils.js
+++ js/utils.js
@@ -237,7 +237,6 @@ NexT.utils = {
       // TOC item animation navigate.
       link.addEventListener('click', event => {
         event.preventDefault();
-        var target = document.getElementById(event.currentTarget.getAttribute('href').replace('#', ''));
         var offset = target.getBoundingClientRect().top + window.scrollY;
         window.anime({
           targets  : document.scrollingElement,
